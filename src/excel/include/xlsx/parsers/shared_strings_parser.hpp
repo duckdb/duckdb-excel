d481fbe93981f355fdad9b4fac8d7b67a7da7e8a
Max Gabrielsson <max@gabrielsson.com>
2025-03-24 15:43:37 +0100

    ignore phonetic text

--- src/excel/include/xlsx/parsers/shared_strings_parser.hpp
+++ src/excel/include/xlsx/parsers/shared_strings_parser.hpp
@@ -45,6 +45,9 @@ private:
 				state = State::T;
 				// Enable text handling
 				EnableTextHandler(true);
+			} else if (MatchTag("rPh", name)) {
+				// We dont include phonetic text in the strings
+				state = State::RPH;
 			}
 			break;
 		case State::T:
@@ -80,6 +83,11 @@ private:
 				Stop(false);
 			}
 			break;
+		case State::RPH:
+			if (MatchTag("rPh", name)) {
+				state = State::SI;
+			}
+			break;
 		default:
 			break;
 		}
@@ -89,7 +97,7 @@ private:
 	}
 
 private:
-	enum class State : uint8_t { START, SST, SI, T };
+	enum class State : uint8_t { START, SST, SI, T, RPH };
 	State state = State::START;
 	vector<char> data;
 };
